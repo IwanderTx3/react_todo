a9b513c7daa6532c281de581681f3b10378932f4
Van <Van@MyPlan.works>
2018-07-17 13:33:01 -0500

    created starter style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className ='NewItemBox'>
+        <div className='NewTask'><input className='NewTaskInput' type="text" id="newTask" placeholder="Enter New Task" /><button id='addTask'>Create Task</button></div>
+        </div>
+        <div className='PendingItemBox'>
+          <div className='titleBar'><p>Pending Items</p></div>
+          <ul>
+            <li className='taskList'>
+              <div className='tasktitle'>A Place Holder Task</div>
+              <div className='blank'></div>
+              <button className='complete'></button>
+              <button className='delete'></button>
+            </li>
+          </ul>
+        </div>
+        <div className='CompletedItemBox'>
+          <div className='titleBar'><p>Completed Items</p></div>
+          <ul>
+            <li className='taskList'>
+              <div className='tasktitle'>A Place Holder Task</div>
+              <div className='blank'></div>
+              <button className='complete'></button>
+              <button className='delete'></button>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
